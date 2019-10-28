ae9618471eebcb7ffd3ac1373ce236474484ccdb
Madenski <bryan.madenski@edu.itescia.fr>
2019-10-28 16:40:13 +0100

    Auto stash before rebase of "refs/heads/master"
    tableau ok

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,15 @@ public class Main {
      System.out.println("   A B C D E F G H I J");
      for (int line = 0;line<10;line=line +1){
          String affichage = " ";
+<<<<<<< HEAD
          for (int col= 0;col<10;col=col+1){
              tab [col] [line]= '~';
                     affichage= affichage+tab [col][line] + ' ';
+=======
+         for (int column= 0;column<10;column=column+1){
+             tab [column] [line]= '~';
+                    affichage= affichage+tab [column][line] + ' ';
+>>>>>>> master
          }
          System.out.println(line+" "+ affichage);
      }
@@ -22,7 +28,25 @@ public class Main {
         System.out.println("Welcome to BattleShip");
         //welcome message
 
+<<<<<<< HEAD
 createTable();
+=======
+
+        
+
+
+
+
+
+
+
+        // Gameboard creation
+
+
+
+
+        createTable();
+>>>>>>> master
 
 
     }
