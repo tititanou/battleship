48049d3b01c0edef1291e86270d7fdbd849c95bb
Tania <tania.rojas@edu.itescia.fr>
2019-10-28 12:25:42 +0100

    Message d'accueil

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("test");
-        //coucou
+        System.out.println("Welcome to BattleShip");
+        //msg de bienvenue
     }
 }
