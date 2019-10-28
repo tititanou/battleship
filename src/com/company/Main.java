fd36d4355a70bf46ca69864d43569b72373fb4e6
Tania <tania.rojas@edu.itescia.fr>
2019-10-28 17:00:57 +0100

    split les fonctions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,35 @@
 package com.company;
-
 import com.sun.security.jgss.GSSUtil;
-
 public class Main {
- // table
- static void createTable(){
-     char tab [][] = new char[10][10];
-     System.out.println("   A B C D E F G H I J");
-     for (int line = 0;line<10;line=line +1){
-         String affichage = " ";
-         for (int column= 0;column<10;column=column+1){
-             tab [column] [line]= '~';
-                    affichage= affichage+tab [column][line] + ' ';
-         }
-         System.out.println(line+" "+ affichage);
-     }
- }
+    // table
+    static void createTable(){
+        char tab [][] = new char[10][10];
+        System.out.println("   A B C D E F G H I J");
+        for (int line = 0;line<10;line=line +1){
+            String affichage = " ";
+            for (int column= 0;column<10;column=column+1){
+                tab [column] [line]= '~';
+                affichage= affichage+tab [column][line] + ' ';
+            }
+            System.out.println(line+" "+ affichage);
+        }
+    }
+    //static void displayTable(){
+    //    System.out.println(line+" "+ affichage);
+    //}
     public static void main(String[] args) {
-
         // write your code here
         System.out.println("Welcome to BattleShip");
         //welcome message
-
-
-        
-
-
-
-
-
-
-
         // Gameboard creation
-
-
-
-
         createTable();
+        /*test=(int) (Math.random()*6);
+        System.out.println(test);
+         */
 
 
     }
 }
+
 
 
