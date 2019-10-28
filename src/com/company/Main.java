acc8988eda02723209a0a77f10743b583b32ab47
Madenski <bryan.madenski@edu.itescia.fr>
2019-10-28 16:28:53 +0100

    tableau 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,13 @@ public class Main {
  static void createTable(){
      char tab [][] = new char[10][10];
      System.out.println("   A B C D E F G H I J");
-     for (int row = 0;row<10;row=row +1){
+     for (int line = 0;line<10;line=line +1){
          String affichage = " ";
          for (int col= 0;col<10;col=col+1){
-             tab [col] [row]= '~';
-                    affichage= affichage+tab [col][row] + ' ';
+             tab [col] [line]= '~';
+                    affichage= affichage+tab [col][line] + ' ';
          }
-         System.out.println(row+" "+ affichage);
+         System.out.println(line+" "+ affichage);
      }
  }
     public static void main(String[] args) {
@@ -21,13 +21,6 @@ public class Main {
         // write your code here
         System.out.println("Welcome to BattleShip");
         //welcome message
-
-      /*  char tableauCaractere[] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
-
-
-        for (int i = 1; i < tableauCaractere.length; i = i + 1) {
-            System.out.println("Pour l'emplacement " + i + " du tableau nous avons = " + tableauCaractere[i]);
-        }*/
 
 createTable();
 
