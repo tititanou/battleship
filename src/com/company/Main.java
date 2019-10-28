dffbb274d49270edac54f9745414e17f42cc47e3
Kevin <r_kevin@hotmail.fr>
2019-10-28 14:11:38 +0100

    Tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,36 @@
 package com.company;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to BattleShip");
         //welcome message
+
+        char battleFieldLine0 [] =  {' ',' ',' ','A',' ','B',' ','C',' ','D',' ','E',' ','F',' ','G',' ','H',' ','I',' ','J'};
+        char battleFieldLine1 [] =  {'1',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine2 [] =  {'2',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine3 [] =  {'3',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine4 [] =  {'4',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine5 [] =  {'5',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine6 [] =  {'6',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine7 [] =  {'7',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine8 [] =  {'8',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine9 [] =  {'9',' ',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        char battleFieldLine10 [] =  {'1','0',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~',' ','~'};
+        System.out.println(battleFieldLine0);
+        System.out.println(battleFieldLine1);
+        System.out.println(battleFieldLine2);
+        System.out.println(battleFieldLine3);
+        System.out.println(battleFieldLine4);
+        System.out.println(battleFieldLine5);
+        System.out.println(battleFieldLine6);
+        System.out.println(battleFieldLine7);
+        System.out.println(battleFieldLine8);
+        System.out.println(battleFieldLine9);
+        System.out.println(battleFieldLine10);
 
 
 
