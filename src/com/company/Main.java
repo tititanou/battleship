418bc2a655657fcf28732a849b744083a87b42ed
Madenski <bryan.madenski@edu.itescia.fr>
2019-10-28 12:40:15 +0100

    welcome mesage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to BattleShip");
-        //msg de bienvenue
+        //welcome mesage
+
+
     }
+
 }
