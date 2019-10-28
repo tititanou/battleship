c127555a64576b54d59461aefcc9c8433e86b5c6
Madenski <bryan.madenski@edu.itescia.fr>
2019-10-28 16:17:52 +0100

    nouveau table

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,49 +3,36 @@ package com.company;
 import com.sun.security.jgss.GSSUtil;
 
 public class Main {
-
+ // table
+ static void createTable(){
+     char tab [][] = new char[10][10];
+     System.out.println("   A B C D E F G H I J");
+     for (int row = 0;row<10;row=row +1){
+         String affichage = " ";
+         for (int col= 0;col<10;col=col+1){
+             tab [col] [row]= '~';
+                    affichage= affichage+tab [col][row] + ' ';
+         }
+         System.out.println(row+" "+ affichage);
+     }
+ }
     public static void main(String[] args) {
+
         // write your code here
         System.out.println("Welcome to BattleShip");
         //welcome message
 
-
-        char battleFieldLine0[] = {' ', ' ', ' ', 'A', ' ', 'B', ' ', 'C', ' ', 'D', ' ', 'E', ' ', 'F', ' ', 'G', ' ', 'H', ' ', 'I', ' ', 'J'};
-        char battleFieldLine1[] = {'1', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine2[] = {'2', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine3[] = {'3', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine4[] = {'4', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine5[] = {'5', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine6[] = {'6', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine7[] = {'7', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine8[] = {'8', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine9[] = {'9', ' ', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        char battleFieldLine10[] = {'1', '0', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~', ' ', '~'};
-        System.out.println(battleFieldLine0);
-        System.out.println(battleFieldLine1);
-        System.out.println(battleFieldLine2);
-        System.out.println(battleFieldLine3);
-        System.out.println(battleFieldLine4);
-        System.out.println(battleFieldLine5);
-        System.out.println(battleFieldLine6);
-        System.out.println(battleFieldLine7);
-        System.out.println(battleFieldLine8);
-        System.out.println(battleFieldLine9);
-        System.out.println(battleFieldLine10);
-        // Gameboard creation
-
-
-
-      /*  int MyTable [] = new int[10];
-        double tableauDouble[] = {0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
-        char tableauCaractere[] = {'a','b','c','d','e','f','g'};
-        System.out.println(tableauCaractere);*/
-        char tableauCaractere[] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+      /*  char tableauCaractere[] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
 
         for (int i = 1; i < tableauCaractere.length; i = i + 1) {
-            System.out.println("À l'emplacement " + i + " du tableau nous avons = " + tableauCaractere[i]);
-        }
+            System.out.println("Pour l'emplacement " + i + " du tableau nous avons = " + tableauCaractere[i]);
+        }*/
+
+createTable();
+
+
     }
 }
+
 
