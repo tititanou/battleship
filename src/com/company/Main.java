82c124046716ee7585491df8ccbfd9019bfdf1cb
Tania <tania.rojas@edu.itescia.fr>
2019-10-29 10:39:39 +0100

    Player Board

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,50 @@ package com.company;
 import com.sun.security.jgss.GSSUtil;
 public class Main {
     // table
-    static void createTable(){
-        char tab [][] = new char[10][10];
-        System.out.println("   A B C D E F G H I J");
-        for (int line = 0;line<10;line=line +1){
-            String affichage = " ";
-            for (int column= 0;column<10;column=column+1){
-                tab [column] [line]= '~';
-                affichage= affichage+tab [column][line] + ' ';
+
+    static void initBoard(char tab[][]) {
+            //createTable
+            for (int line = 0; line < tab.length; line = line + 1) {
+                for (int column = 0; column < tab[0].length; column = column + 1) {
+                    tab[line][column]='~';
+                }
             }
-            System.out.println(line+" "+ affichage);
+
+            // createShips
+
+
+    }
+
+    static void displayBoard(char tab[][]) {
+        System.out.println("    A B C D E F G H I J");
+        System.out.println("   ---------------------");
+        for (int line = 0; line < tab.length; line = line + 1) {
+            String display = " ";
+            for (int column = 0; column < tab[0].length; column = column + 1) {
+                display = display + tab[line][column] + " ";
+            }
+            display = (line+1) +"|"+ display +"|";
+            if (line<tab.length-1){
+                display=" "+display;
+            }
+            System.out.println( display );
         }
+
+        System.out.println("   ---------------------");
     }
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome to BattleShip");
         //welcome message
+
+        char playerBoard[][]= new char[10][10];
+
+        initBoard(playerBoard);
+
+        displayBoard(playerBoard);
         // Gameboard creation
-        createTable();
+        //createTable();
 
         /*test=(int) (Math.random()*6);
         System.out.println(test);
